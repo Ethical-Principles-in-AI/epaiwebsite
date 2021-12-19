c50e6f9eb1507b6d84db93d72f28c242342ccd19
BuildTools <chasemcdougall@hotmail.com>
2021-12-19 15:24:39 -0500

    Updates

---
+++ precache-manifest.7fc5a105b30c6f66a227d7dc89f637e6.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cff53780b5676d23ef2ce91c27f20e7c",
+    "url": "/epaiwebsite/index.html"
+  },
+  {
+    "revision": "89c44f4f99ced9bca033",
+    "url": "/epaiwebsite/static/css/main.d2c60c0b.chunk.css"
+  },
+  {
+    "revision": "ce53b952ffbd8e364498",
+    "url": "/epaiwebsite/static/js/2.b836d60d.chunk.js"
+  },
+  {
+    "revision": "9e80ed6ee19e2ff5e581d429458bb084",
+    "url": "/epaiwebsite/static/js/2.b836d60d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89c44f4f99ced9bca033",
+    "url": "/epaiwebsite/static/js/main.84c7deef.chunk.js"
+  },
+  {
+    "revision": "8d4043946b3253f94e8e",
+    "url": "/epaiwebsite/static/js/runtime-main.ff9fbbb0.js"
+  },
+  {
+    "revision": "d3b4a99fb8041032774daad19360deea",
+    "url": "/epaiwebsite/static/media/Dullerud.d3b4a99f.png"
+  },
+  {
+    "revision": "bd61513fe3386f8cbe51d5ff258ffd2a",
+    "url": "/epaiwebsite/static/media/Ethics_panel.bd61513f.jpg"
+  },
+  {
+    "revision": "875a669fe2cc961383342312ad5eac24",
+    "url": "/epaiwebsite/static/media/anya.875a669f.jpg"
+  },
+  {
+    "revision": "2ea818da3d8224fbb86e68d037f0ed50",
+    "url": "/epaiwebsite/static/media/chase.2ea818da.jpg"
+  },
+  {
+    "revision": "386fad0ddfc7deb0dcdc0a8ff961d389",
+    "url": "/epaiwebsite/static/media/chris.386fad0d.jpg"
+  },
+  {
+    "revision": "70cb0d7c4abfe198952249716b6d820d",
+    "url": "/epaiwebsite/static/media/club_fair.70cb0d7c.png"
+  },
+  {
+    "revision": "3d8fea314dceef97a5206810e0af55e0",
+    "url": "/epaiwebsite/static/media/dhruv.3d8fea31.jpg"
+  },
+  {
+    "revision": "56a6d631c937066119180cc91b88c47d",
+    "url": "/epaiwebsite/static/media/dullerud_profile.56a6d631.jpeg"
+  },
+  {
+    "revision": "17957a1e2bc6c7e8dd07d085b6b7786f",
+    "url": "/epaiwebsite/static/media/email.17957a1e.jpg"
+  },
+  {
+    "revision": "6a5d6ea3854bcf5aa0b7c6e3cdde061b",
+    "url": "/epaiwebsite/static/media/emily.6a5d6ea3.jpg"
+  },
+  {
+    "revision": "c05552f38d8f974598c04e18c0ad0763",
+    "url": "/epaiwebsite/static/media/epai_banner.c05552f3.png"
+  },
+  {
+    "revision": "9b75d2929557624e9d68aa7c05524779",
+    "url": "/epaiwebsite/static/media/epai_logo.9b75d292.png"
+  },
+  {
+    "revision": "758346eaee044ff24fa7b4e74a316369",
+    "url": "/epaiwebsite/static/media/epai_mid_banner.758346ea.png"
+  },
+  {
+    "revision": "2f67545ea76ed60e8fd58c4bd4f573b1",
+    "url": "/epaiwebsite/static/media/ethics_panel_page.2f67545e.png"
+  },
+  {
+    "revision": "58783a75eaa075f8a82761c2bcfaee2c",
+    "url": "/epaiwebsite/static/media/facial-recognition.58783a75.png"
+  },
+  {
+    "revision": "8c29842a2901cf485aeb110cdf3e73d2",
+    "url": "/epaiwebsite/static/media/fizzah.8c29842a.png"
+  },
+  {
+    "revision": "27974500e7dd19155e0921c73addcec8",
+    "url": "/epaiwebsite/static/media/gary.27974500.jpg"
+  },
+  {
+    "revision": "54c129efea6b2bc53c39842745a15efb",
+    "url": "/epaiwebsite/static/media/instagram.54c129ef.jpg"
+  },
+  {
+    "revision": "3010a4213fb816fde226daf454d94cc0",
+    "url": "/epaiwebsite/static/media/jasmine.3010a421.jpg"
+  },
+  {
+    "revision": "9f5c07125a47b9f0dea30517e3934f46",
+    "url": "/epaiwebsite/static/media/jong.9f5c0712.jpg"
+  },
+  {
+    "revision": "e31413b5fbd9e5133d34e8bda9c5b085",
+    "url": "/epaiwebsite/static/media/mic.e31413b5.png"
+  },
+  {
+    "revision": "86a2bca2ffd124109eead6b1f6f5f11c",
+    "url": "/epaiwebsite/static/media/robert_title_slide.86a2bca2.jpg"
+  },
+  {
+    "revision": "3d934d364c482533266718cf86318c1c",
+    "url": "/epaiwebsite/static/media/rss_logo.3d934d36.png"
+  },
+  {
+    "revision": "5431c681ba7e6c0af35ca1573ef5747a",
+    "url": "/epaiwebsite/static/media/sarina.5431c681.jpg"
+  },
+  {
+    "revision": "c679859e3f77b09ede0fef24e9a0df05",
+    "url": "/epaiwebsite/static/media/signup.c679859e.jpg"
+  },
+  {
+    "revision": "375702573204c431972fc6fc731dd226",
+    "url": "/epaiwebsite/static/media/soden.37570257.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cb3227b37826aa1ea865f78eb904fec0.js"
+  "/epaiwebsite/precache-manifest.7fc5a105b30c6f66a227d7dc89f637e6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/epaiwebsite/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
